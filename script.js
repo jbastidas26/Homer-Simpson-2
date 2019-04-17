dde7d0a25b914a60613b8c191e45f9ec82be8448
jbastidas26 <43036752+jbastidas26@users.noreply.github.com>
2019-04-17 08:35:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+$("#profile-picture").click(function(){
+    alert("whyyyyyy!");
+$("#interests").css("background-color" , "pink");
+
+});
+
+
+$("#interests").click(function(){
+    alert("Leave Me Alone!");
+$("#interests").css("background-color" , "gold");
+});
+
+$("#wall").click(function(){
+    alert("Ohhh NO!");
+$("#wall").css("background-color" , "purple");
+});
+
+$(".bart").click(function(){
+    alert("Damnnnnn"); 
+    
+    
+$("#header").css("background-color" , "green");
+});
+
+$(".ned").click(function(){
+    alert("wanna cookie");
+$(".ned").css("background-color" , "yellow");
+});
+
+$(".maggie").click(function(){
+    alert("googoo gaaagaaa");
+$(".maggie").css("background-color" , "blue");
+});
